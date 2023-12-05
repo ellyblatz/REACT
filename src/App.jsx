ff4a5937e091bce31bf1a3385abb45a9e89a07c2
Elly <leblatz4@gmail.com>
2023-12-05 04:14:30 -0500

    map over data, shows puppy names on page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react'
 import './App.css'
+import {puppyList} from './data.js'
+
+
 
 function App() {
 
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
   return (
-    <>
-      <div>
-        
-      </div>
-      
-    
-     
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
